66db84e18b4e608da2c4c72ab3d5123b18a9dd63
Ren Wei <wadefelix@gmail.com>
2017-10-07 18:27:28 +0800

    使用background.js来发送请求

--- background.js
+++ background.js
@@ -4,15 +4,6 @@ browser.contextMenus.create({
     contexts: ["link"],
 });
 
-/*
-"{\"jsonrpc\":\"2.0\",\"id\":\"qwer\",\"method\":\"aria2.addUri\",\"params\":[[\"
-http:\\/\\/data.vod.itc.cn\\/?rb=1&prot=1&key=jbZhEJhlqlUN-Wj_HEI8BjaVqKNFvDrn&prod=flash&pt=1&new=\\/244\\/118\\/oggXgtJv24awoAcC0o1Cz5.mp4
-\"],{\"out\":\"央视主播雷人囧事一箩筐[高清版].mp4\"}]}"
-"{"jsonrpc":"2.0","id":"qwer","method":"aria2.addUri","params":[["
-http://data.vod.itc.cn/?rb=1&amp;prot=1&amp;key=jbZhEJhlqlUN-Wj_HEI8BjaVqKNFvDrn&amp;prod=flash&amp;pt=1&amp;new=/244/118/oggXgtJv24awoAcC0o1Cz5.mp4
-"],{"out":"test"}]}"
-http://data.vod.itc.cn/?rb=1&amp;prot=1&amp;key=jbZhEJhlqlUN-Wj_HEI8BjaVqKNFvDrn&amp;prod=flash&amp;pt=1&amp;new=/244/118/oggXgtJv24awoAcC0o1Cz5.mp4
-*/
 function S2A2_xmlhttpRequest(dlurl, filename) {
   try {
     var oReq = new XMLHttpRequest();
@@ -44,12 +35,14 @@ browser.contextMenus.onClicked.addListener((info, tab) => {
         // Always HTML-escape external input to avoid XSS.
         const safeUrl = escapeHTML(info.linkUrl);
         
-        /*S2A2_xmlhttpRequest(safeUrl, 'test');*/
-        
-        
         browser.tabs.sendMessage(tab.id, safeUrl);
     }
 });
+
+browser.runtime.onMessage.addListener(function (info) {
+  S2A2_xmlhttpRequest(info.url, info.filename);
+});
+
 
 // https://gist.github.com/Rob--W/ec23b9d6db9e56b7e4563f1544e0d546
 function escapeHTML(str) {
--- content-script.js
+++ content-script.js
@@ -1,33 +1,9 @@
 
-function S2A2_xmlhttpRequest(dlurl) {
-  try {
-  
+function S2A2_getfilename(dlurl) {
     var filename = window.top.prompt("Send To Aria2 另存为文件名","");
-    
     if (filename.length==0) return;
-    
-    var oReq = new XMLHttpRequest();
-    /*oReq.addEventListener("load", function(a1, a2, a3) {
-      console.log('xhr.load: %s, %s, %s', a1, a2, a3);
-    });*/
-
-    jsonreq = {'jsonrpc':'2.0',
-               'id':'qwer',
-               'method':'aria2.addUri',
-               'params': [[dlurl],{'out':filename}]}
-    jsonreqstr = JSON.stringify(jsonreq);
-    // open synchronously
-    oReq.open("post","http://192.168.1.4:6800/jsonrpc",false);
-
-    // send
-    var res = oReq.send(jsonreqstr);
-    //console.log('xhr result: %s', res);
-  } catch(e) {
-    debugger;
-    console.warn('could not send ajax request, reason %s', e.toString());
-  }
+    browser.runtime.sendMessage({"url": dlurl, "filename":filename});
 }
 
-browser.runtime.onMessage.addListener(S2A2_xmlhttpRequest);
-//window.addEventListener("contextmenu", notifyClnuExtensionBackgroudjs);
+browser.runtime.onMessage.addListener(S2A2_getfilename);
 
