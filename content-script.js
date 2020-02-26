6769b43aa85b7902295d93656c81ea5d347ce11c
Ren Wei <wadefelix@gmail.com>
2020-02-26 11:11:34 +0800

    再弹一个窗是否继续

--- content-script.js
+++ content-script.js
@@ -1,8 +1,14 @@
 
 function S2A2_getfilename(dlurl) {
     var filename = window.top.prompt("[Send To Aria2] Save As Filename","");
-    if (filename.length==0) return;
-    browser.runtime.sendMessage({"url": dlurl, "filename":filename});
+    var sendto = true;
+    if (filename==null || filename.length==0) {
+        var sendtowithblank = window.top.prompt("Send to Aria2 Directly? \"Cancel\" to cancel this Action.");
+        if (!sendtowithblank) sendto = false;
+    }
+    if (sendto) {
+        browser.runtime.sendMessage({"url": dlurl, "filename":filename});
+    }
 }
 
 browser.runtime.onMessage.addListener(S2A2_getfilename);
